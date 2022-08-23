db7ad248a618be0fea1cf140963cf53acf0a1422
Tushar Mathur <tusharmath@gmail.com>
2022-08-23 08:21:57 +0530

    Revert "refactor: Change implementation for StringDSL"
    This reverts commit 4f335fc2e2c9d1ff37d4003103e22423860ea3d2.

--- compose/src/main/scala/compose/dsl/StringDSL.scala
+++ compose/src/main/scala/compose/dsl/StringDSL.scala
@@ -6,23 +6,23 @@ import compose.Lambda.make
 
 trait StringDSL[-A, +B] { self: A ~> B =>
   final def ++[A1 <: A, B1 >: B](other: A1 ~> B1)(implicit ev: B1 <:< String): A1 ~> B1 =
-    self >>> make[B1, B1] { Textual.Concat(other.compile) }
+    make[A, B] { Textual.Concat(self.compile, other.compile) }
 
   final def contains[A1 <: A](other: A1 ~> String)(implicit ev: B <:< String): A1 ~> Boolean =
-    self >>> make[B, Boolean](Textual.Contains(other.compile))
+    make[A1, Boolean](Textual.Contains(self.compile, other.compile))
 
   final def endsWith[A1 <: A](other: A1 ~> String)(implicit ev: B <:< String): A1 ~> Boolean =
-    self >>> make[B, Boolean](Textual.EndsWith(other.compile))
+    make[A1, Boolean](Textual.EndsWith(self.compile, other.compile))
 
   final def length(implicit ev: B <:< String): A ~> Int =
-    self >>> make[B, Int](Textual.Length)
+    make[A, Int](Textual.Length(self.compile))
 
   final def lowerCase(implicit ev: B <:< String): A ~> String =
-    self >>> make[B, String](Textual.LowerCase)
+    make[A, String](Textual.LowerCase(self.compile))
 
   final def startsWith[A1 <: A](other: A1 ~> String)(implicit ev: B <:< String): A1 ~> Boolean =
-    self >>> make[B, Boolean](Textual.StartsWith(other.compile))
+    make[A1, Boolean](Textual.StartsWith(self.compile, other.compile))
 
   final def upperCase(implicit ev: B <:< String): A ~> String =
-    self >>> make[B, String](Textual.UpperCase)
+    make[A, String](Textual.UpperCase(self.compile))
 }
